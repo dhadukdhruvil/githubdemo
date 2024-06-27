a3389688b0dc99a6229126f17e1c5cfac5fccee4
dhadukdhruvil <dhruvildhaduk625@gmail.com>
2024-06-27 09:22:19 +0530

    hello world

---
+++ operators.py
@@ -0,0 +1,54 @@
+# print ("hello world")
+
+
+
+
+
+# operators
+
+print("________operators")
+
+a=10
+b=20
+total=a+b
+# total=a-b
+# total=a*b
+# total=a/b
+# total=a%b
+# total=b%a
+
+
+# print(a+b)
+# total=a>b
+# total=a<b
+# total=a>=b
+# total=a<=b
+# total=a==b
+# total=a!=b
+
+# total=(a>=b and a>b)
+# total=(a<=b and a<b)
+# total=not(a<=b and a<b)
+# print("Total is:",total)
+# print(5+58-4*89)
+
+# userinput
+# name="Dhruvil"
+# name=input("Enter Your name::")
+# print("Welcome to my site mr."+name)
+
+# num1=int(input("Enter your num1:"))
+# num2=int(input("Enter your num2:"))
+# print(type(num1))
+# print(num1+num2)
+
+
+
+# # python 2
+# name="Dhruvil"
+# age=19
+# print("Your name is "+name+ "Your age is "+str(age))
+# python 3
+# print("Your name is {} Your age is {}".format(name,age))
+# python 3.5
+# print(f"Your name is {name} your age is {age}")
---
+++ strings.py
@@ -0,0 +1,88 @@
+# fname="Dhruvil"
+# lname="Dhaduk"
+# space=" "
+# full_name=fname+space+lname
+# print(full_name)
+
+
+# fname="Dhruvil"
+# lname="Dhaduk"
+# space=" "
+# print(fname+space+lname)
+
+# fname="Dhruvil"
+# lname="Dhaduk"
+# print(fname+"\t"+lname)
+# num1=int(input("enter your Num1::"))
+# num2=int(input("enter your Num2::"))
+# print(num1+num2)
+# print(type(num1))
+# name="Python"
+# print(name[0])
+# print(name[-1])
+# print(name[0:3])
+# print(name[0::2])
+# print(name[-5:-2])
+
+
+# methods
+name="PYTHON TESTING"
+astr="*************"
+# print(len(name))
+# print(name.upper())
+# print(name.lower())
+# print(name.title())
+# print(name.count("n"))
+first=name.find("n")
+# print(first)
+# print(name.find("n",first+1))
+# print(name.replace("P","L"))
+print(astr+name.strip()+astr)
+# print(astr+name.lstrip()+astr)
+# print(astr+name.rstrip()+astr)
+# print(name.center(50,"*"))
+# name[0]="T" error bcz string is immutable 
+
+
+
+
+# print("Hellos")
+
+
+# fname="Dhruvil"
+# lname="Dhaduk"
+# space=" "
+# print(fname+space+lname)
+
+# fname="Dhruvil"
+# lname="Dhaduk"
+# print(fname+"\t"+lname)
+# num1=int(input("enter your Num1::"))
+# num2=int(input("enter your Num2::"))
+# print(num1+num2)
+# print(type(num1))
+# name="Python"
+# print(name[0])
+# print(name[-1])
+# print(name[0:3])
+# print(name[0::2])
+# print(name[-5:-2])
+
+
+# methods
+name="PyTHon TESing"
+astr="*************"
+# print(len(name))
+# print(name.upper())
+# print(name.lower())
+# print(name.title())
+# print(name.count("n"))
+# first=name.find("n")
+# print(first)
+# print(name.find("n",first+1))
+# print(name.replace("P","H"))
+# print(astr+name.strip()+astr)
+# print(astr+name.lstrip()+astr)
+# print(astr+name.rstrip()+astr)
+# print(name.center(50,"*"))
+# name[0]="T" error bcz string is immutable 
